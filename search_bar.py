b119582f0a31eb1d05295547869e71b52573b3ba
Chirag Popli <34339354+chirag-popli@users.noreply.github.com>
2021-06-16 23:11:16 +0530

    Update search_bar.py

--- search_bar.py
+++ search_bar.py
@@ -13,7 +13,7 @@ class Signup(db.Model):
     date_time = db.Column(db.DateTime)
 @app.route("/<string:key>")
 def search(key):
-    return "chirag"
+    return "chirag {}".format(key)
     res=Signup.query.filter_by(username=key).first()
     return jsonify({'name':res.username})
 
